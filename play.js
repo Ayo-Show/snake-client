12f26fdc7b16829250ad8fe0f067caff6d50b820
Ayo-Show <sowunmi30@gmail.com>
2021-11-03 19:01:40 -0400

    stdin setup

--- play.js
+++ play.js
@@ -3,5 +3,28 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = require('./client');
 
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  stdin.on("data", handleUserInput);
+  
+  const handleUserInput = function(data){
+    if (data === '\u0003') {
+      process.exit();
+    };
+  
+  };
+  
+  return stdin;
+  
+}  
+  
+
 console.log("Connecting ...");
-connect();
+
